fec7446cdd0e54ff0ad3539cc71768baeae3d044
Felipe Sagredo <fnsagred@gmail.com>
2014-04-25 19:31:15 -0400

    new rails project (middleware)
    cree un nuevo proyecto para que trabajemos sobre ese

---
+++ Middleware/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Middleware::Application.config.secret_key_base = '054aaa53a76fb782a7e2b125a39d8f8159d7d3c0a19023faea514acc8c84db6978047ba5dae13c4ae5b6e66d716011a93b2be2b9b63bd331d49bfbcd6aab64b6'
